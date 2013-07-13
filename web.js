a26f3382de358bc45421a496cdd2dcbb44d6df94
Prakash Wadhwani <ieyesight@live.com>
2013-07-13 10:32:37 +0000

    web.js part 3

--- web.js
+++ web.js
@@ -2,13 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('/bitstarter/index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
+var buf = new Buffer(256)
+buf = fs.readFileSync('/bitstarter/index.html','utf8');
 
-buf = new Buffer(256)
-data = buf.toString()
+var data = buf.toString()
 
 app.get('/', function(request, response) {
   response.send('data');
@@ -18,4 +15,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
- 
